2aea20667cf5550f5d2fa7d4261be5ef951ad61f
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2022-12-20 09:18:06 +0530

    Word Counter App modified with regular expression logic and completed

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ let textBox = document.querySelector(`.textBox`);
 let wordCount = 0;
 let charCount = 0;
 
-textBox.addEventListener(`keydown`, (e) => {
-    console.log(`${e.keyCode} ${e.key} is pressed`);
+textBox.addEventListener(`input`, (e) => {
+    // console.log(`${e.keyCode} ${e.key} is pressed`);
 
-    if (e.keyCode == 8 && charCount > 0) {
-        console.log(`Backspace is trapped`);
-        charCount -= 1;
-        document.querySelector(`.totalChar`).innerText = charCount.toString();
-        return;
-    }
+    // if (e.keyCode == 8 && charCount > 0) {
+    //     console.log(`Backspace is trapped`);
+    //     charCount -= 1;
+    //     document.querySelector(`.totalChar`).innerText = charCount.toString();
+    //     return;
+    // }
 
-    if (e.keyCode != 8 && e.keyCode != 32) {
-        charCount += 1;
-        document.querySelector(`.totalChar`).innerText = charCount.toString();
-        console.log(charCount);
-    }
+    // if (e.keyCode != 8 && e.keyCode != 32) {
+    //     charCount += 1;
+    //     document.querySelector(`.totalChar`).innerText = charCount.toString();
+    //     console.log(charCount);
+    // }
     
-    if (e.keyCode == 32) {
-        wordCount += 1;
-        document.querySelector(`.totalWords`).innerText = wordCount.toString();
-    }
+    // if (e.keyCode == 32) {
+    //     wordCount += 1;
+    //     document.querySelector(`.totalWords`).innerText = wordCount.toString();
+    // }
+
+    console.log(textBox.value.length);
+    document.querySelector(`.totalChar`).innerText = textBox.value.length;
+    
+    let wordSet = textBox.value;
+    wordSet = wordSet.match( /\w+/g );
+    console.log(wordSet);
+    
+    document.querySelector(`.totalWords`).innerText = wordSet.length;
 });
 
